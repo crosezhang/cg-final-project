4d3a9354a30876dd76cdb1d34e59d4d0c58bf6a4
Allen Shen <ashen8@jhu.edu>
2023-12-08 14:25:43 -0500

    edited create_test_file.py

--- fastas/create_test_file.py
+++ fastas/create_test_file.py
@@ -2,36 +2,32 @@ import random
 import sys
 
 input_file_1 = sys.argv[1]
-input_file_1_num = sys.argv[2]  # number of sequences for input file 1
+input_file_1_num = int(sys.argv[2])  # number of sequences for input file 1
 input_file_2 = sys.argv[3]
-input_file_2_num = sys.argv[4]  # number of sequences for input file 2
-output_file = sys.argv[5]
+input_file_2_num = int(sys.argv[4])  # number of sequences for input file 2
+sequence_length = int(sys.argv[5])
 
-
-def extract_and_write_random_lines(file_1, input_1, file_2, input_2, output_file):
-    def get_random_lines(file_path, num_sequences, sequence_length):
+def extract_and_write_random_lines(file_1, input_1, file_2, input_2, seq_length):
+    def get_random_lines(file_path, num_sequences, seq_length):
         with open(file_path, 'r') as file:
             lines = file.readlines()
-            sequences = [''.join(random.sample(lines, sequence_length)) for _ in range(num_sequences)]
-            return [f">{file_path.removesuffix('.fasta')}_Sequence_{i}\n{seq}" for i, seq in
+            sequences = [''.join(random.sample(lines, seq_length)) for _ in range(num_sequences)]
+            return [f">{file_path.removesuffix('.fasta')}_sequence_{i}\n{seq}" for i, seq in
                     enumerate(sequences, start=1)]
 
-    input_1 = int(input_1)
-    input_2 = int(input_2)
-    sequence_length = 6
-
-    lines_from_file_1 = get_random_lines(file_1, input_1, sequence_length)
-    lines_from_file_2 = get_random_lines(file_2, input_2, sequence_length)
+    lines_from_file_1 = get_random_lines(file_1, input_1, seq_length)
+    lines_from_file_2 = get_random_lines(file_2, input_2, seq_length)
 
     combined_sequences = lines_from_file_1 + lines_from_file_2
     random.shuffle(combined_sequences)  # Shuffling the combined sequences
 
-    with open(output_file, 'w') as output_file:
+    output_file_path = f"{input_file_1.removesuffix('.fasta')}_{input_1}_{input_file_2.removesuffix('.fasta')}_{input_2}.txt"
+
+    with open(output_file_path, 'w') as output_file:
         for seq in combined_sequences:
             output_file.write(seq)
 
     return "Sequences written to the output file in random order and FASTA format."
 
 
-print(extract_and_write_random_lines(input_file_1, input_file_1_num, input_file_2, input_file_2_num,
-                                     output_file))
+print(extract_and_write_random_lines(input_file_1, input_file_1_num, input_file_2, input_file_2_num, sequence_length))

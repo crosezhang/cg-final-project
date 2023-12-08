e69df0e2ea4ac3daaee68a0c9461fc160908dbc4
crosezhang <crosezhang@gmail.com>
2023-12-08 12:20:12 -0500

    added full kraken output

--- cont-pipeline.sh
+++ cont-pipeline.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-# krakenuniq --db minikraken --report-file $2 $1
-phyloligo.py -d JSD -i $1 -o $2 -c 64 --method joblib
-phyloselect.py -i $2 -m kmedoids --noX -o $3 -p 20
+krakenuniq --db minikraken --report-file $2 $1 --output $2
+# phyloligo.py -d JSD -i $1 -o $2 -c 64 --method joblib
+# phyloselect.py -i $2 -m kmedoids --noX -o $3 -p 20

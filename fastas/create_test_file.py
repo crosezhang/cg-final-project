9bafdc27e641919ab498803ea2293e9ebb8040f4
Allen Shen <ashen8@jhu.edu>
2023-12-08 14:52:46 -0500

    edited create_test_fiile.py

--- fastas/create_test_file.py
+++ fastas/create_test_file.py
@@ -7,17 +7,17 @@ input_file_2 = sys.argv[3]
 input_file_2_num = int(sys.argv[4])  # number of sequences for input file 2
 sequence_length = int(sys.argv[5])
 
-
 def extract_and_write_random_lines(file_1, input_1, file_2, input_2, seq_length):
     def get_random_lines(file_path, num_sequences, seq_length):
         with open(file_path, 'r') as file:
             lines = file.readlines()
             selected_lines = []
-            for _ in range(num_sequences):
+            for seq_num in range(num_sequences):
                 start_index = random.randint(0, max(0, len(lines) - seq_length))
                 selected_seq = lines[start_index:start_index + seq_length]
                 sequence = ''.join(selected_seq)
-                sequence_label = f"{file_path.removesuffix('.fasta')}Lines{start_index + 1}to{start_index + seq_length}"
+                file_label = file_path.removesuffix('.fasta')
+                sequence_label = f"{file_label}_Sequence_{seq_num + 1}(lines {start_index + 1} to {start_index + seq_length} ) "
                 selected_lines.append(f">{sequence_label}\n{sequence}")
             return selected_lines
 
@@ -34,6 +34,5 @@ def extract_and_write_random_lines(file_1, input_1, file_2, input_2, seq_length)
             output_file.write(seq)
 
     return f"Sequences written to {output_file_path} in random order and FASTA format."
-
 
 print(extract_and_write_random_lines(input_file_1, input_file_1_num, input_file_2, input_file_2_num, sequence_length))

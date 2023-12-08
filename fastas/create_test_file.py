50a7317c5151803c8c5f232b22f4eec296ae7db3
Allen Shen <ashen8@jhu.edu>
2023-12-08 16:59:56 -0500

    added reference genomes

--- fastas/create_test_file.py
+++ fastas/create_test_file.py
@@ -1,10 +1,11 @@
+import os
 import random
 import sys
 
 input_file_1 = sys.argv[1]
-input_file_1_num = int(sys.argv[2])  # number of sequences for input file 1
+input_file_1_num = int(sys.argv[2])
 input_file_2 = sys.argv[3]
-input_file_2_num = int(sys.argv[4])  # number of sequences for input file 2
+input_file_2_num = int(sys.argv[4])
 sequence_length = int(sys.argv[5])
 
 def extract_and_write_random_lines(file_1, input_1, file_2, input_2, seq_length):
@@ -16,8 +17,8 @@ def extract_and_write_random_lines(file_1, input_1, file_2, input_2, seq_length)
                 start_index = random.randint(0, max(0, len(lines) - seq_length))
                 selected_seq = lines[start_index:start_index + seq_length]
                 sequence = ''.join(selected_seq)
-                file_label = file_path.removesuffix('.fasta')
-                sequence_label = f"{file_label}_sequence_{seq_num + 1} lines {start_index + 1} to {start_index + seq_length}"
+                file_label = os.path.basename(file_path).removesuffix('.fasta')
+                sequence_label = f"{file_label} Sequence{seq_num + 1} Lines {start_index + 1} to {start_index + seq_length}"
                 selected_lines.append(f">{sequence_label}\n{sequence}")
             return selected_lines
 
@@ -27,13 +28,14 @@ def extract_and_write_random_lines(file_1, input_1, file_2, input_2, seq_length)
     combined_sequences = lines_from_file_1 + lines_from_file_2
     random.shuffle(combined_sequences)
 
-    output_file_path = f"{file_1.removesuffix('.fasta')}_{input_1}_{file_2.removesuffix('.fasta')}_{input_2}.fasta"
+    file_1_name = os.path.basename(file_1).removesuffix('.fasta')
+    file_2_name = os.path.basename(file_2).removesuffix('.fasta')
+    output_file_path = f"{file_1_name}_{input_1}_{file_2_name}_{input_2}.fasta"
 
     with open(output_file_path, 'w') as output_file:
         for seq in combined_sequences:
             output_file.write(seq)
 
     return f"Sequences written to {output_file_path} in random order and FASTA format."
-
 
 print(extract_and_write_random_lines(input_file_1, input_file_1_num, input_file_2, input_file_2_num, sequence_length))
